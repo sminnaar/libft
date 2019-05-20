046192f8cd32845f926b6fcb5aca45fd83f2867f
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-05-20 14:24:53 +0200

    Updating libft with 17 .c files, incomplete Makefile and libft.h

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sminnaar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 12:38:01 by sminnaar          #+#    #+#             */
+/*   Updated: 2019/05/20 13:48:58 by sminnaar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+		char	*str;
+		unsigned long	i;
+
+		i = 0;
+		str = (char *)malloc(sizeof(char)*(ft_strlen(s1) + 1));
+		if (s[i] == NULL)
+			return (NULL);
+		while (s[i])
+		{
+			str[i] == s1[i];
+			i++;
+		}
+		str[i] == '\0';
+		return (str);
+}
---
+++ libft.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sminnaar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/19 15:20:31 by sminnaar          #+#    #+#             */
+/*   Updated: 2019/05/20 14:16:14 by sminnaar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}							t_list;
+
+int				ft_atoi(char *str);
+int				ft_isalnum(int c);
+int				ft_isalpha(int a);
+int				ft_isascii(int a);
+int				ft_isdigit(int d);
+int				ft_isprint(int p);
+void			*ft_memset(void *b, int c, unsigned long len);
+void			ft_putchar(char c);
+void			ft_putnbr((int nbr);
+void			ft_putstr(char *c);
+void			ft_strclr(char *s);
+int				ft_strcmp(const char *s1, const *s2);
+char			*ft_strcpy(char *dst, const char *src);
+unsigned long	ft_strlen(const char *s);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+
+
+#endif
